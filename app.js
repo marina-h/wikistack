fa672254eed06faf391e0f5f43595dede763ca56
Marina Hoashi <marinahoashi@gmail.com>
2017-04-28 16:23:14 -0400

    initial files

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var router = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+// var socketio = require('socket.io');
+
+app.use(morgan('dev'));
+
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', router);
