8ec09b4de7ab0ecf56a8dfad05b183e301b3c74f
Jin Lin <jinlin314@hotmail.com>
2017-04-28 17:28:09 -0400

    added user.js wiki.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+const wikiRouter = require('./wiki');
+const userRouter = require('./user');
 
 module.exports = function() {
   router.get('/', function(req, res) {
     res.render('index');
     // res.send('hello world')
+
   })
+  router.use('/wiki', wikiRouter);
+  // or, in one line: router.use('/wiki', require('./wiki'));
+  router.use('/user', userRouter);
+  // or, in one line: router.use('/wiki', require('./wiki'));
+
 
   return router;
 }
---
+++ routes/user.js
@@ -0,0 +1,15 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+module.exports = function() {
+  router.get('/', function(req, res) {
+    res.render('index/');
+    // res.send('hello world')
+  })
+
+
+
+
+  return router;
+}
